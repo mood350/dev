d2f4417b1db864d3d8efde0ab7d79acdf285c6cd
mood350 <princiuso350@gmail.com>
2025-03-01 13:24:45 +0000

    Suppression d'un default dans utilisateur

--- utilisateur/models.py
+++ utilisateur/models.py
@@ -7,7 +7,7 @@ class Utilisateur(models.Model):
     prenoms = models.CharField(max_length=100)
     telephone = models.CharField(max_length=20)
     email = models.EmailField()
-    pasword = models.CharField(max_length=50, default="password")
+    pasword = models.CharField(max_length=50)
 
     def __str__(self):
         return f"{self.nom} {self.prenoms}"
