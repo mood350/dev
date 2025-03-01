6045d913ca9ff35f5f9dc0ca6e99cdbb32e24e32
mood350 <princiuso350@gmail.com>
2025-03-01 13:20:50 +0000

    Mise à jour des fichiers Admin.py

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,1 +1,36 @@
 from django.contrib import admin
+from .models import Hotel, Etage, ChambreHotel, Reservation
+
+
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'adresse', 'telephone', 'email', 'description', 'image')
+    list_filter = ('nom', 'adresse', 'telephone', 'email', 'description', 'image')
+    search_fields = ('nom', 'adresse', 'telephone', 'email', 'description', 'image')
+    list_per_page = 10
+
+admin.site.register(Hotel, HotelAdmin)
+
+class EtageAdmin(admin.ModelAdmin):
+    list_display = ('numero_etage',)
+    list_filter = ('numero_etage',)
+    search_fields = ('numero_etage',)
+    list_per_page = 10
+
+admin.site.register(Etage, EtageAdmin)
+
+class ChambreHotelAdmin(admin.ModelAdmin):
+    list_display = ('numeroChambre', 'type', 'etage', 'prix', 'petit_dejeuner', 'disponibilite', 'description', 'image')
+    list_filter = ('type', 'etage', 'prix', 'petit_dejeuner', 'disponibilite', 'description', 'image')
+    search_fields = ('numeroChambre', 'type', 'etage', 'prix', 'petit_dejeuner', 'disponibilite', 'description', 'image')
+    list_per_page = 10
+
+admin.site.register(ChambreHotel, ChambreHotelAdmin)
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('client', 'chambre', 'nombre_personnes', 'nombre_nuits', 'petit_dejeuner', 'montant', 'date_reservation')
+    list_filter = ('client', 'chambre', 'nombre_personnes', 'nombre_nuits', 'petit_dejeuner', 'montant', 'date_reservation')
+    search_fields = ('client', 'chambre', 'nombre_personnes', 'nombre_nuits', 'petit_dejeuner', 'montant', 'date_reservation')
+    list_per_page = 10
+
+admin.site.register(Reservation, ReservationAdmin)
+
--- utilisateur/admin.py
+++ utilisateur/admin.py
@@ -2,6 +2,28 @@ from django.contrib import admin
 from .models import Client, Personnel, Administrateur
 
 # Register your models here.
-admin.site.register(Client)
-admin.site.register(Personnel)
-admin.site.register(Administrateur)
+
+
+class PersonnelAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'prenoms', 'telephone', 'email', 'fonction', 'date_embauche', 'salaire')
+    list_filter = ('fonction', 'date_embauche')
+    search_fields = ('nom', 'prenoms', 'telephone', 'email')
+    list_per_page = 10
+
+admin.site.register(Personnel, PersonnelAdmin)
+
+class AdministrateurAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'prenoms', 'telephone', 'email', 'fonction', 'date_embauche', 'salaire')
+    list_filter = ('fonction', 'date_embauche')
+    search_fields = ('nom', 'prenoms', 'telephone', 'email')
+    list_per_page = 10
+
+admin.site.register(Administrateur, AdministrateurAdmin)
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'prenoms', 'telephone', 'email')
+    list_filter = ('nom', 'prenoms', 'telephone', 'email')
+    search_fields = ('nom', 'prenoms', 'telephone', 'email')
+    list_per_page = 10
+
+admin.site.register(Client, ClientAdmin)
